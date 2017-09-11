073564d09247f22892ba170f75bb6f2dda840fe4
Andrew Scott <andrew.scott@intellitect.com>
2017-09-11 16:11:37 -0600

    Feature 1

--- src/GitDemo/Program.cs
+++ src/GitDemo/Program.cs
@@ -11,6 +11,7 @@ namespace GitDemo
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Feature 1!");
         }
     }
 }
